b37597f9644a234c077ac2530600785e94378357
bdevops99 <bdevops99@gmail.com>
2023-06-10 23:11:33 +0530

    Jenkins - pipeline

---
+++ Jenkinsfile
@@ -0,0 +1,26 @@
+pipeline {
+
+agent {
+node {
+label 'workstation'
+}
+}
+parameters {
+choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+string(name: 'component', defaultValue: '', description: 'component name')
+}
+stages {
+ stage('Anisible'){
+ steps {
+  sh 'ansible-playbook -i ${component}-dev.panda4u.online, roboshop.yml -e ansible_user=centos -e ansible_password=DevOps321 -e role_name=${component} -e env=${env}'
+ }
+ }
+
+}
+post {
+  always {
+     cleanWs()
+    }
+  }
+
+}
---
+++ vars/terraform.groovy
@@ -0,0 +1,31 @@
+def call(int buildNumber) {
+    pipeline {
+
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+        }
+        stages {
+            stage('Terraform INIT') {
+                steps {
+                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                }
+            }
+            stage('Terraform APPLY') {
+                steps {
+                    sh 'terraform apply -var-file=env-${env}/main.tfvars -auto-approve'
+                }
+            }
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+}
